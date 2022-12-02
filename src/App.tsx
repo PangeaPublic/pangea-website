1f0fba17046c6279d7b43bfcda5747a68b792a21
Bence A. Tóth <tothab@gmail.com>
2022-12-02 17:16:14 +0100

    Added main and footer embryo

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,32 @@ import "./App.css";
 const App = () => (
   <div className="site">
     <SiteHeader />
-    <main className="site-main">Main</main>
-    <footer className="site-footer">Footer</footer>
+    <main className="site-main">
+      <div className="site-main-inner-wrapper">
+        <h1>Lorem ipsum</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          dolores laboriosam, esse dignissimos fugit architecto officiis in,
+          porro ipsam, blanditiis labore ex aspernatur hic voluptatem nam
+          reprehenderit commodi laudantium repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, expedita
+          consequatur odit hic suscipit aut! Provident quisquam, cupiditate
+          consequatur neque vero adipisci delectus cum cumque dolor laborum,
+          nostrum fugiat sint!
+        </p>
+      </div>
+    </main>
+    <footer className="site-footer">
+      <div className="site-footer-inner-wrapper">
+        <div>
+          <div>Copyright stuff</div>
+          <div>Contact</div>
+          <div>Social stuff</div>
+        </div>
+      </div>
+    </footer>
   </div>
 );
 
