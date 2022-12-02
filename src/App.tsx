de86f3a95e66091ed2182ab13ca435e549c54293
Bence A. Tóth <tothab@gmail.com>
2022-12-02 14:34:35 +0100

    Basic menu styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
+import LogoPrimary from "./pangea-logo-primary-120.png";
+
+import "./App.css";
+
 const App = () => (
-  <div>
-    <h1>Pangea Website will come here</h1>
+  <div className="site">
+    <header className="site-header">
+      <div className="site-header-inner-wrapper">
+        <div className="logo-wrapper">
+          <img src={LogoPrimary} alt="Pangea" />
+        </div>
+        <nav className="desktop-menu">
+          <ul>
+            <li>
+              <a href="/" className="selected">
+                Who we are
+              </a>
+            </li>
+            <li>
+              <a href="/">What we do</a>
+            </li>
+            <li>
+              <a href="/">Get involved</a>
+            </li>
+            <li>
+              <a href="/">Pangea press</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <main className="site-main">Main</main>
+    <footer className="site-footer">Footer</footer>
   </div>
 );
 
