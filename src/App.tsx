e712d651b6760608e3bce4917aee43ed64cf214d
Bence A. Tóth <tothab@gmail.com>
2023-01-21 13:03:42 +0100

    Implemented site footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { FaFacebook, FaEnvelope, FaLinkedin } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaEnvelope,
+  FaInstagram,
+  FaTiktok,
+  FaYoutube,
+} from "react-icons/fa";
 
 import SiteHeader from "./SiteHeader";
 
@@ -38,26 +44,68 @@ const App = () => (
             <p className="copyright">
               © {copyrightYear} Pangea. All rights reserved.
             </p>
+            <address>
+              Prague LGBT Center,
+              <br />
+              Železná 492/18,
+              <br />
+              110 00 Staré Město, Czechia
+            </address>
           </div>
         </div>
-        <div>
+        <div className="contact">
           <div>
-            <p className="social-heading">Contact Pangea</p>
+            <p className="social-heading">Contact us</p>
+            <a
+              className="email-address"
+              href="mailto:pangea.resistance@gmail.com"
+            >
+              <span className="social-icons">
+                <FaEnvelope />
+              </span>
+              <span className="email-address-text">
+                pangea.resistance@gmail.com
+              </span>
+            </a>
+          </div>
+          <div>
+            <p className="social-heading">Follow us</p>
             <nav className="social-icons">
               <ul>
                 <li>
-                  <a href="/">
-                    <FaEnvelope />
-                  </a>
-                </li>
-                <li>
-                  <a href="/">
+                  <a
+                    href="https://www.facebook.com/pangea.resistance"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <FaFacebook />
                   </a>
                 </li>
                 <li>
-                  <a href="/">
-                    <FaLinkedin />
+                  <a
+                    href="https://www.instagram.com/pangea_prague/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaInstagram />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.tiktok.com/@pangea_prague"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaTiktok />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.youtube.com/channel/UCkqMpSO9SzY9WTiZwvQC8yw"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaYoutube />
                   </a>
                 </li>
               </ul>
