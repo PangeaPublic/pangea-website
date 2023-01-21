131622a364d472ee65fb52bac9bbc0f02f581fac
Bence A. Tóth <tothab@gmail.com>
2023-01-21 16:58:58 +0100

    Page components and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import { Routes, Route } from "react-router-dom";
 import SiteHeader from "./SiteHeader";
+import WhoWeAre from "./WhoWeAre";
+import WhatWeDo from "./WhatWeDo";
+import Press from "./Press";
+import Resources from "./Resources";
+import GetInvolved from "./GetInvolved";
 import Footer from "./Footer";
 
 import "./App.css";
@@ -8,19 +14,13 @@ const App = () => (
     <SiteHeader />
     <main className="site-main">
       <div className="site-main-inner-wrapper">
-        <h1>Lorem ipsum</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-          dolores laboriosam, esse dignissimos fugit architecto officiis in,
-          porro ipsam, blanditiis labore ex aspernatur hic voluptatem nam
-          reprehenderit commodi laudantium repudiandae.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, expedita
-          consequatur odit hic suscipit aut! Provident quisquam, cupiditate
-          consequatur neque vero adipisci delectus cum cumque dolor laborum,
-          nostrum fugiat sint!
-        </p>
+        <Routes>
+          <Route path="/" element={<WhoWeAre />} />
+          <Route path="/what-we-do" element={<WhatWeDo />} />
+          <Route path="/press" element={<Press />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/get-involved" element={<GetInvolved />} />
+        </Routes>
       </div>
     </main>
     <Footer />
---
+++ src/Resources.tsx
@@ -0,0 +1,19 @@
+const GetInvolved = () => (
+  <>
+    <h1>Resources</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat dolores
+      laboriosam, esse dignissimos fugit architecto officiis in, porro ipsam,
+      blanditiis labore ex aspernatur hic voluptatem nam reprehenderit commodi
+      laudantium repudiandae.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, expedita
+      consequatur odit hic suscipit aut! Provident quisquam, cupiditate
+      consequatur neque vero adipisci delectus cum cumque dolor laborum, nostrum
+      fugiat sint!
+    </p>
+  </>
+);
+
+export default GetInvolved;
