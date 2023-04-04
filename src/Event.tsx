d7ab2fa1b53a76d8391416b33bfe7f40f5312e7c
Bence A. Tóth <tothab@gmail.com>
2023-04-04 20:12:10 +0200

    Event structure and styles

--- src/Event.tsx
+++ src/Event.tsx
@@ -2,6 +2,14 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchEvent } from "./Api";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import {
+  FaRegClock,
+  FaCalendarAlt,
+  FaMapMarkerAlt,
+  FaExternalLinkAlt,
+  FaCalendarPlus,
+  FaFacebookSquare,
+} from "react-icons/fa";
 
 import Loader from "./Loader";
 
@@ -27,27 +35,87 @@ const EventPage = () => {
   }
 
   return (
-    <article className="article">
+    <article className="event">
       <h1>{event.fields.title}</h1>
-      {documentToReactComponents(event.fields.description as Document)}
-      <section>
-        <h3>When</h3>
-        <time dateTime={event.fields.time}>
-          {new Date(event.fields.time).toLocaleDateString()}{" "}
-          {new Date(event.fields.time).toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-          })}
-        </time>
-      </section>
-      <section>
-        <h3>Where</h3>
-        <a href={event.fields.locationUrl}>{event.fields.location}</a>
-      </section>
-      <div>
-        <a href={event.fields.registrationUrl}>Register</a>
-        <a href={event.fields.facebookEventLink}>Facebook event</a>
+      <div className="formatted-content">
+        {documentToReactComponents(event.fields.description as Document)}
       </div>
+      <section className="where-when">
+        <div>
+          <h3>When</h3>
+          <p>
+            <span className="icon">
+              <FaCalendarAlt />
+            </span>
+            <time dateTime={event.fields.time}>
+              <span>{new Date(event.fields.time).toLocaleDateString()}</span>
+            </time>
+          </p>
+          <p>
+            <span className="icon">
+              <FaRegClock />
+            </span>
+            <time dateTime={event.fields.time}>
+              <span>
+                {new Date(event.fields.time).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </span>
+            </time>
+          </p>
+        </div>
+        {event.fields.location && (
+          <div>
+            <h3>Where</h3>
+            <p>
+              <span className="icon">
+                <FaMapMarkerAlt />
+              </span>
+              {event.fields.locationUrl ? (
+                <a
+                  href={event.fields.locationUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {event.fields.location}
+                  <span className="external-link-icon">
+                    <FaExternalLinkAlt />
+                  </span>
+                </a>
+              ) : (
+                <span>{event.fields.location}</span>
+              )}
+            </p>
+          </div>
+        )}
+        <div className="links">
+          {event.fields.registrationUrl && (
+            <a
+              href={event.fields.registrationUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="icon">
+                <FaCalendarPlus />
+              </span>
+              Register
+            </a>
+          )}
+          {event.fields.facebookEventLink && (
+            <a
+              href={event.fields.facebookEventLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="icon">
+                <FaFacebookSquare />
+              </span>
+              Check out on Facebook
+            </a>
+          )}
+        </div>
+      </section>
     </article>
   );
 };
--- src/Events.tsx
+++ src/Events.tsx
@@ -18,11 +18,13 @@ const EventLink = ({ event }: EventLinkProps) => (
       <div className="title-and-date">
         <h2>{event.fields.title}</h2>
         <time dateTime={event.fields.time}>
-          {new Date(event.fields.time).toLocaleDateString()}{" "}
-          {new Date(event.fields.time).toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-          })}
+          <span>{new Date(event.fields.time).toLocaleDateString()}</span>
+          <span>
+            {new Date(event.fields.time).toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </span>
         </time>
       </div>
     </article>
